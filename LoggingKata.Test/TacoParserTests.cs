7a049a99e37b3d3dc8a9428f803196c91e5392a4
Amy Galbicsek <acgalbicsek@gmail.com>
2023-02-23 11:41:13 -0900

    TACO PARSER COMPLETE I THINK

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -30,14 +30,31 @@ namespace LoggingKata.Test
             //       each representing a TacoBell location
 
             //Arrange
+            var tacoParserInstance = new TacoParser();
 
             //Act
+            var actual = tacoParserInstance.Parse(line);
 
             //Assert
+            Assert.Equal(expected, actual.Location.Longitude);
         }
 
 
         //TODO: Create a test ShouldParseLatitude
+        [Theory]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
+
+        public void ShouldParseLatitude(string line, double expected)
+        {
+            //Arrange
+            var tacoParserInstance = new TacoParser();
+
+            //Act
+            var actual = tacoParserInstance.Parse(line);
+
+            //Assert
+            Assert.Equal(expected, actual.Location.Latitude);
+        }
 
     }
 }
